bce9b773b52966d297ce2c39a8f2ba30faa83a34
Hector Mendoza <hec1702@gmail.com>
2019-09-08 13:27:26 -0500

    Grid View. Assets added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
+// Flutter code sample for material.AppBar.1
+
+// This sample shows an [AppBar] with two simple actions. The first action
+// opens a [SnackBar], while the second action navigates to a new page.
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/map.png'),
-              Text('Testing Card')
-            ],
-          ),
+    return MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('¿Más Favoritos?'),
+        backgroundColor: Colors.red,
+      ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 3,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+          return Center(
+            child: Image.asset('assets/user.png'),
+          );
+        }),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.red,
+        child: Container(
+          height: 70,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerRight,
+          child: Text('Siguiente',
+              style: TextStyle(fontSize: 16, color: Colors.white)),
         ),
       ),
     );
